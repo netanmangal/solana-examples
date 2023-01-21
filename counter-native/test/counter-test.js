e07448f6b2cfcda1db1b131a2ffb13f240a4fdda
netanmangal <imnetanmangal@gmail.com>
2023-01-21 17:06:46 +0530

    Add: counter tests

--- counter-native/test/counter-test.js
+++ counter-native/test/counter-test.js
@@ -1,3 +1,4 @@
+// run the file with `-- timeout 60000` if error of timeout comes
 const { expect } = require("chai");
 require("dotenv").config();
 const web3 = require("@solana/web3.js");
@@ -8,7 +9,7 @@ describe("Check Counter Contract", () => {
     let senderKeyPair;
 
     let progPublicKey = new web3.PublicKey("A1gr4PLeUPTcsEwAuksqhk7FuBzbcRyfbmRBYLwtXJxy");
-    let counterDataKey = ""
+    let counterDataKeyPair;
 
     before(async () => {
         if (progPublicKey == null) {
@@ -23,18 +24,14 @@ describe("Check Counter Contract", () => {
         senderKeyPair = web3.Keypair.fromSecretKey(
             Uint8Array.from(privateSeedKey)
         );
-    })
 
-    it("Counter increases", async () => {
         // allocate data program id
         let transaction = new web3.Transaction();
-
-        let counterKeyPair = new web3.Keypair();
-        console.log(counterKeyPair.publicKey.toBase58());
+        counterDataKeyPair = new web3.Keypair();
 
         let counterIx = web3.SystemProgram.createAccount({
             fromPubkey: senderKeyPair.publicKey,
-            newAccountPubkey: counterKeyPair.publicKey,
+            newAccountPubkey: counterDataKeyPair.publicKey,
             lamports: await connection.getMinimumBalanceForRentExemption(8),
             space: 8,
             programId: progPublicKey
@@ -42,12 +39,27 @@ describe("Check Counter Contract", () => {
 
         transaction.add(counterIx);
 
+        await web3.sendAndConfirmTransaction(
+            connection,
+            transaction,
+            [senderKeyPair, counterDataKeyPair],
+            {
+                skipPreflight: true
+            }
+        )
+
+        console.log(`Counter data address - ${counterDataKeyPair.publicKey.toBase58()}`);
+    });
+
+    async function ping_contract() {
+        let transaction = new web3.Transaction();
+
         let instructionBuffer = Buffer.from(new Uint32Array([0]));
 
         let transactionInst = new web3.TransactionInstruction({
             keys: [
                 {
-                    pubkey: counterKeyPair.publicKey,
+                    pubkey: counterDataKeyPair.publicKey,
                     // pubkey: new web3.PublicKey("95qTB38tF1kfupaKXx3uqwMxtkZXMPh91etWq47yZzVL"),
                     isSigner: false,
                     isWritable: true
@@ -62,11 +74,40 @@ describe("Check Counter Contract", () => {
         await web3.sendAndConfirmTransaction(
             connection,
             transaction,
-            [senderKeyPair, counterKeyPair],
+            [senderKeyPair],
             {
                 skipPreflight: true
             }
         )
+    }
 
+    it("Counter increases +1", async () => {
+        await ping_contract();
+    }).timeout(60000);
+
+    async function read_counter_account_data() {
+        const dataAccount = await connection.getAccountInfo(counterDataKeyPair.publicKey);
+
+        const borshCounterStruct = borsh.struct([
+            borsh.u64("count")
+        ]);
+
+        return borshCounterStruct.decode(dataAccount.data)
+    }
+
+    it("Reading contract - value 1", async () => {
+        const { count } = await read_counter_account_data();
+        expect(count.toString()).to.equal("1");
+    }).timeout(60000);
+
+    it("Counter increases +3", async () => {
+        await ping_contract();
+        await ping_contract();
+        await ping_contract();
+    }).timeout(180000);
+
+    it("Reading contract - value 4", async () => {
+        const { count } = await read_counter_account_data();
+        expect(count.toString()).to.equal("4");
     }).timeout(60000);
 })
